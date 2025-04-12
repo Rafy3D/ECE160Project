af0ea2d4937aa7bddf286480966868af7d3d3589
Rafael Pinheiro <rpinheiro1@umassd.edu>
2025-04-11 23:16:21 -0400

    update

--- ECE160Project/src.c
+++ ECE160Project/src.c
@@ -11,32 +11,39 @@ typedef struct recipe {
 	char name[32];
 	char desc[1028];
 };
+/*Variables & Declarations*/
 char dir[MAX_PATH];
 char opDir[MAX_PATH];
 void newRecipe();
 void removeRecipe(int n);
 void openRecipe(int n);
 void listRecipes();
+void setDir();
+void scanInput();
+int recCount;
+
 
 int main(void) {
-	_getcwd(dir, sizeof(dir));
-	snprintf(dir, sizeof(dir), "%s\\Recipes", dir);
-	snprintf(opDir, sizeof(opDir), "%s\\*", dir);
-	_chdir(dir);
-	printf("This program stores and recalls user inputted recipes. Input the number to the left of an entry to view its content. \nInput 'n' to create a new recipe. Input 'd' to delete a recipe. Input 'e' to edit an existing recipe.\n");
-	listRecipes();
+	setDir();
+	while (1) {
+		listRecipes();
+		scanInput();
+	}
 }
 
 void listRecipes() {
 	HANDLE hFind = INVALID_HANDLE_VALUE;
 	WIN32_FIND_DATA ffd;
 	hFind = FindFirstFileA(opDir, &ffd);
+	system("cls");
+	printf("This program stores and recalls user inputted recipes. Input the number to the left of an entry to view its content. \nInput 'n' to create a new recipe. Input 'd' to delete a recipe. Input 'e' to edit an existing recipe.\n");
 	printf("\tRECIPES:\n");
+	recCount = 0;
 	do {
 		if (hFind != "INVALID_HANDLE_VALUE") {
 			char* name = ffd.cFileName;
-			
-			printf(" %s \n", name);
+			printf("%d\t%s \n", recCount, name);
+			recCount++;
 		}
 	} while (FindNextFileA(hFind,&ffd)!=0);
 	FindClose(hFind);
@@ -58,6 +65,44 @@ void newRecipe() {
 	system(filepath);
 }
 void removeRecipe(int n) {}
+void setDir() {
+	_getcwd(dir, sizeof(dir));
+	snprintf(dir, sizeof(dir), "%s\\Recipes", dir);
+	snprintf(opDir, sizeof(opDir), "%s\\*.txt", dir);
+	printf("\n\n\n%d\n\n\n", _chdir(dir));
+	if (_chdir(dir) == -1) { _mkdir(dir); }
+	_chdir(dir);
+}
+void scanInput() {
+	char select;
+	printf("Input your selection: ");
+	scanf_s(" %c", &select);
+	switch(select){
+	case 's':
+		//select recipe
+		break;
+	case 'n':
+		//new recipe
+		newRecipe();
+		break;
+	case 'e':
+		//edit recipe
+		break;
+	case 'r':
+		//remove recipe
+		break;
+	default:
+		printf("Invalid input");
+		break;
+	}
+
+}
+
+
+
+
+
+
 
 //thangs to do
 //gotta add error detection
