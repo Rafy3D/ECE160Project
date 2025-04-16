0cce9d6a9cdc618b2b2350c19fab3b2669d056e8
BuildTools <unconfigured@null.spigotmc.org>
2025-04-16 01:09:02 -0400

    final

--- ECE160Project/src.c
+++ ECE160Project/src.c
@@ -7,9 +7,9 @@
 #include <conio.h>
 
 /*Variables & Declarations*/
-char dir[MAX_PATH];
-char opDir[MAX_PATH];
-char* recList[64] = {NULL};
+char dir[1028];
+char opDir[1028];
+char* recList[64] = {0};
 void newRecipe();
 void removeRecipe(int n);
 void openRecipe(int n);
@@ -32,11 +32,11 @@ void listRecipes() {
 	WIN32_FIND_DATA ffd;					//stores the data collected from each file scanned. Most important to our use case is the file name
 	hFind = FindFirstFileA(opDir, &ffd);	//Initialize the handle to the first file found in the Recipes directory, here the directory path passed to the function is opDir, which is the working directiory + /*.txt appended to tell the find file function to only scan for all .txt files. file data is saved to ffd
 	recCount = 0;							//counter that will serve as the index for each file
-	memset(recList, NULL, sizeof(recList));	//recList is an array that contains the names of all the files that will be scanned. this funciton clears that array to initialize it
+	memset(recList, 0, sizeof(recList));	//recList is an array that contains the names of all the files that will be scanned. this funciton clears that array to initialize it
 	printf("This program stores and recalls user inputted recipes. Input the number to the left of an entry to view its content. \nInput 'v' to view a recipe, 'n' to create a new recipe, 'd' to delete a recipe,'e' to edit an existing recipe. Close program with ESC.\n");
 	printf("\tRECIPES:\n");
 	do {
-		if (hFind != INVALID_HANDLE_VALUE) { //this if should only excecute if the directory is not empty, this doesnt work for now and if you run it with the Recipes folder empty it prints a bunch of crazy stuff
+		if (hFind != INVALID_HANDLE_VALUE) { //excecutes if directory is not empty
 			char* name = ffd.cFileName;										// sets the variable name to the name of the current file scanned including the .txt extension
 			printf("%d\t%s \n", recCount, name);							//prints the recipe name along with its index number
 			recList[recCount] = malloc(strlen(name) + 1);					//allocates memory to the current pointer in recList to scanned recipe name
@@ -71,11 +71,10 @@ void openRecipe(int n) {
 }
 void newRecipe() {
 	FILE* dat;
-	char newRecName[_MAX_FNAME];
+	char newRecName[_MAX_FNAME-4];
 	char filename[_MAX_FNAME];
-	char filepath[32000];
+	char filepath[1028];
 	printf("Enter name of new recipe: ");
-	fflush(stdin);
 	fgets(newRecName, sizeof(newRecName), stdin);													//scans user input for recipe name
 	newRecName[strcspn(newRecName, "\n")] = 0;														//fgets appends (or reads?) the newline character from hitting enter to submit your input and thats a problem so this function just strips away that \n character
 	snprintf(filename, sizeof(filename), "%s.txt", newRecName);										//takes the name the user inputted and adds .txt to the end so that the resulting file that is created lower down in this function will be a .txt file
@@ -104,6 +103,7 @@ void removeRecipe(int n) {
 		_getch();
 	}
 	else {
+		scanf_s("%*[^\n]%*c");
 		printf("Canceling deletion. Press any key to continue");
 		_getch();
 	}
@@ -114,43 +114,66 @@ void setDir() {
 	//sets the working directory to the Recipes folder so that everything is nice and organized
 	_getcwd(dir, sizeof(dir));								//gets current directory, should always be project folder
 	snprintf(dir, sizeof(dir), "%s\\Recipes", dir);			//appends "Recipes" to the current working directory path and saves the resulting path to the dir variable defined at the beginning
-	snprintf(opDir, sizeof(opDir), "%s\\*.txt", dir);		//this one appends "*.txt" to the end of the path from the previous step. this is necessary later for the function that scans the Recipes directory for all the recipe files
+	snprintf(opDir, sizeof(opDir), "%s\\*.txt", dir);		//this one appends "*.txt" to the end of the path from the previous step and saves to opDir. this is necessary later for the function that scans the Recipes directory for all the recipe files
 	if (_chdir(dir) == -1) { _mkdir(dir); }					//checks if Recipes folder exists. If not, it is created.
 	_chdir(dir);											//finally sets the current working directory to the Recipes folder
 }
 void scanInput() {
-	/*This function scans the user's desired input. it first scans what action the user wants to perform (as in view recipe or create a new one) then executes the function to perform that action*/
-	printf("Input your selection: ");
-	switch (_getch()) {
+	/*This function first scans what action the user wants to perform (as in view recipe or create a new one), prompts the user for an index, then executes the function to perform that action on the specified file if applicable*/
+	printf("Input your desired action: ");
+	char c;
+	switch (c = _getch()) {
 		int sel;
 	case 'v':
 		//select recipe
-		//need to add detection so that user can only pass int or program crashes
+		printf("%c", c);
 		printf("\nEnter recipe number to view: ");
-		scanf_s("%d", &sel);
-		getchar();
-		openRecipe(sel);
-		break;
+		if (scanf_s("%d", &sel) != 1) {
+			printf("Invalid index. Try again.\n");
+			scanf_s("%*[^\n]%*c");
+			break;
+		}
+		else {
+			scanf_s("%*[^\n]%*c");
+			openRecipe(sel);
+			break;
+		}
 	case 'n':
 		//create new recipe
+		printf("%c", c);
 		printf("\n");
 		newRecipe();
 		break;
 	case 'e':
 		//edit existing recipe
+		printf("%c", c);
 		printf("\nEnter recipe number to edit: ");
-		scanf_s("%d", &sel);
-		getchar();
-		editRecipe(sel);
-		break;
+		if (scanf_s("%d", &sel) != 1) {
+			printf("Invalid index. Try again.\n");
+			scanf_s("%*[^\n]%*c");
+			break;
+		}
+		else {
+			scanf_s("%*[^\n]%*c");
+			editRecipe(sel);
+			break;
+		}
 	case 'd':
 		//remove recipe
+		printf("%c", c);
 		printf("\nEnter recipe number to delete: ");
-		scanf_s("%d", &sel);
-		getchar();
-		removeRecipe(sel);
-		break;
+		if (scanf_s("%d", &sel) != 1) {
+			printf("Invalid index. Try again.\n");
+			scanf_s("%*[^\n]%*c");
+			break;
+		}
+		else {
+			scanf_s("%*[^\n]%*c");
+			removeRecipe(sel);
+			break;
+		}
 	case 27:
+		//27 corresponds to the ESC key. Exits program when ESC is pressed
 		exit(1);
 		break;
 	default:
